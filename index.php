c06e6da004f1dcb7909e0219f6da6bf54a5a7531
arthurroders <80464205+arthurroders@users.noreply.github.com>
2022-07-26 20:44:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
 <html>
+   <head>
+        <title>Trabalho 4 - Integração de Sistemas para Automação</title>
+    </head>
+  <body>
+    <form method="post" action="dados.php">
+      <label> Código da disciplina</label>
+            <input type="text" name="codigo" placeholder="Código">
+            <br>
+      <label> Nome da disciplina</label>
+            <input type="text" name="nome" placeholder="Nome">
+            <br>
+      <label> Carga Horária da disciplina</label>
+            <input type="numer" name="carga" placeholder="Horas">
+            <br>
+      <label> Pré-requisitos da disciplina</label>
+            <input type="text" name="prereq" placeholder="Tópicos">
+            <br>
+      <label>Matérias Equivalentes</label>
+            <input type="text" name="eq" placeholder="Codigo">
+            <br>
+      <label>Ementa da matéria</label>
+            <input type="text" name="ementa" placeholder="Tópicos">
+            <br>
+      <button type="submit">Enviar dados<\button>  
+      <button type="submit">Lista de matérias<\button>  
+        
+       <label>Consultar matéria</label>
+            <input type="text" name="consulta" placeholder="Código">
+            <br>
+         <button type="submit">Consultar<\button>        
+        
+      <\form>
   
-  SEXO
-  
+  <\body>
 </html>
