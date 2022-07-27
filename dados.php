07d9fe341340d6cc145f4f06d7da09013236a375
arthurroders <80464205+arthurroders@users.noreply.github.com>
2022-07-26 21:13:10 -0300

    Create dados.php

---
+++ dados.php
@@ -0,0 +1,29 @@
+<?php
+	$dadosJson = file_get_contents('dados.json');
+	$decodJson = json_decode($dadosJson, true);
+				
+	if(isset($_POST['enviadados'])) {
+		echo "Dados gravados com sucesso!";
+		$values = $_POST;
+		$decodJson["Disciplina"][] = $values;
+		$fp = fopen('dados.json', 'w');
+		fwrite($fp, json_encode($decodJson));
+		fclose($fp);
+	}
+
+	if(isset($_POST['mostradados'])) {
+		print_r($dadosJson);	
+	}
+	
+	if(isset($_POST['consultadados'])) {
+		$codigo = $_POST['codigo'];
+		foreach ($dadosJsonDecodificados["Disciplina"] as $key => $value) {
+			foreach($value as $chave => $valor) {
+    				if($valor == $codigo) {
+					$retorna = json_encode($value); 
+					print_r($retorna);
+				}
+			}		
+		}
+    	}	
+?>
