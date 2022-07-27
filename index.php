212aafc91b25a58f80b77af4efe1f90b14760ba2
arthurroders <80464205+arthurroders@users.noreply.github.com>
2022-07-26 21:12:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,31 +6,32 @@
    <h1> Trabalho Webservice </h1>
   <body>
     <form method="post" action="dados.php">
-      <label> Código da disciplina</label>
+      <label> Código da disciplina:</label>
             <input type="text" name="codigo" placeholder="Código">
             <br>
-      <label> Nome da disciplina</label>
+      <label> Nome da disciplina:</label>
             <input type="text" name="nome" placeholder="Nome">
             <br>
-      <label> Carga Horária da disciplina</label>
+      <label> Carga Horária da disciplina:</label>
             <input type="numer" name="carga" placeholder="Horas">
             <br>
-      <label> Pré-requisitos da disciplina</label>
+      <label> Pré-requisitos da disciplina:</label>
             <input type="text" name="prereq" placeholder="Código">
             <br>
-      <label>Matérias Equivalentes</label>
+      <label>Matérias Equivalentes:</label>
             <input type="text" name="eq" placeholder="Codigo">
             <br>
-      <label>Ementa da Disciplina</label>
+      <label>Ementa da Disciplina:</label>
             <input type="text" name="ementa" placeholder="Tópicos">
             <br>
       <input type="submit" name="enviadados" value="Enviar Dados">
       <input type="submit" name="mostradados" value="Lista de Disciplinas">
         <br>
        <br>
-       <label>Consultar disciplina</label>
-            <input type="submit" name="consultadados" value="Consultar Disciplina">
-            <br>        
+       <label>Consultar disciplina:</label>
+       <input type="text" name="consulta" placeholder="Código">
+       <input type="submit" name="consultadados" value="Consultar Disciplina">
+       <br>        
       </form> 
   </body>
 </html>
