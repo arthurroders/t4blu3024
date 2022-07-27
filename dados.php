d44dd46d2f76c386fee56244d207157ca15dfafd
arthurroders <80464205+arthurroders@users.noreply.github.com>
2022-07-26 22:54:19 -0300

    Update dados.php

--- dados.php
+++ dados.php
@@ -1,11 +1,13 @@
 <?php
 	$dadosJson = file_get_contents('dados.json'); 
+	$decodJson = json_decode($dadosJson,true);
 					
 	if(isset($_POST['enviadados'])) {
 		echo "Dados gravados com sucesso!";
-		$valores[] = $_POST;		
+		$valores = $_POST;	
+		$decodJson[] = $valores;
 		$escrita = fopen('dados.json', 'w');
-		fwrite($escrita, json_encode($valores));
+		fwrite($escrita, json_encode($decodJson));
 		fclose($escrita);
 	}
 
