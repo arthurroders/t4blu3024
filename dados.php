af48a304dd3380d18715fe94a878b94a80b68395
arthurroders <80464205+arthurroders@users.noreply.github.com>
2022-07-26 21:45:34 -0300

    Update dados.php

--- dados.php
+++ dados.php
@@ -1,11 +1,10 @@
 <?php
-	$dadosJson = file_get_contents('dados.json');
-	$decodJson = json_decode($dadosJson, true);
+	$dadosJson = file_get_contents('dados.json'); <!--Pega os dados armazenados no arquivo dados.json-->
+	$decodJson = json_decode($dadosJson, true);  <!--Analisa a string codificada JSON e converte-a em uma variável do PHP.-->
 				
 	if(isset($_POST['enviadados'])) {
 		echo "Dados gravados com sucesso!";
 		$values = $_POST;
-		$decodJson["Disciplina"][] = $values;
 		$fp = fopen('dados.json', 'w');
 		fwrite($fp, json_encode($decodJson));
 		fclose($fp);
@@ -17,12 +16,10 @@
 	
 	if(isset($_POST['consultadados'])) {
 		$codigo = $_POST['codigo'];
-		foreach ($decodJson["Disciplina"] as $key => $value) {
-			foreach($value as $chave => $valor) {
-    				if($valor == $codigo) {
-					$retorna = json_encode($value); 
-					print_r($retorna);
-				}
+		foreach($decodJson[][] => $valor) {
+    			if($valor == $decodJson[][]) {
+				$retorna = json_encode($value); 
+				print_r($retorna);
 			}		
 		}
     	}	
